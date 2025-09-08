955f0daaa54730e8e75e62eb9bb82a6314f74211
Andnet DeBoer <deboerandnet@gmail.com>
2025-09-08 09:49:38 -0400

    Maze class

--- Maze.py
+++ Maze.py
@@ -3,8 +3,8 @@ import numpy as np
 
 class Maze():
 
-    def __init__(self):
-        self.maze = None
+    def __init__(self,filename):
+        self.maze = self.load(filename)
         self.wallCell =[]
         self.freeCell = []
         self.start = None
@@ -15,8 +15,10 @@ class Maze():
 
         with open(filename, 'r') as f:
             lines = f.readlines()
-
         maze = []
 
         for line in lines:
-            row = None
+            row = list(map(int,line.strip().split()))
+            maze.append(row)
+        
+        return np.array(maze)
