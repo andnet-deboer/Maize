0ed2cb092978cc16005d80fa4cd0f58f1fb05df3
Andnet DeBoer <deboerandnet@gmail.com>
2025-09-09 10:24:06 -0400

    added main

---
+++ main.py
@@ -0,0 +1,20 @@
+import recursive_strat
+import maze_manual
+
+
+def main():
+    maze_obj = maze_manual.Maze("MazeFiles/datafile.txt")
+
+    path = []
+    history = []
+
+    maze = maze_manual.Maze("MazeFiles/datafile.txt")
+
+    if recursive_strat.solve_maze(maze_obj.maze, maze_obj.start[0],maze_obj.start[1], path, history):
+        maze.display(history)
+    else:
+        print("No path found")
+
+
+if __name__ == '__main__':
+    main()
