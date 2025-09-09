8c0307913bb8473e63495671b09fd4b3fdff6337
Andnet DeBoer <deboerandnet@gmail.com>
2025-09-09 10:31:53 -0400

    Show path instead of history

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     maze = maze_manual.Maze("MazeFiles/datafile.txt")
 
     if recursive_strat.solve_maze(maze_obj.maze, maze_obj.start[0],maze_obj.start[1], path, history):
-        maze.display(history)
+        maze.display(path)
     else:
         print("No path found")
 
