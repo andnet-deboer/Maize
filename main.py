d502c924360e34ca719cf4b86e18151c5de954c8
Andnet DeBoer <deboerandnet@gmail.com>
2025-09-09 10:48:49 -0400

    Allow user to select full path or direct

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ def main():
 
     maze = maze_manual.Maze("MazeFiles/data50x50.txt")
 
+    hist = int(input("\nEnter 0 for final path enter 1 to visualize full exploration: "))
+
     if recursive_strat.solve_maze(maze_obj.maze, maze_obj.start[0],maze_obj.start[1], path, history):
-        maze.display(path)
+        if hist == 0:
+            maze.display(path)
+        else:
+            maze.display(history)
     else:
         print("No path found")
 
